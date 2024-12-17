589102fc8a3dcd3b773b46918624ee9a67010770
hdjoumdjeu <hermannpougoue1@gmail.com>
2024-12-17 09:21:14 -0600

    chore: update project configuration and fix linting issues
    - Fixed outdated branch by merging `main` into the feature branch to sync with the latest changes.
    - Resolved Jest configuration issues to properly handle CSS and style imports in tests.
    - Configured ESLint to automatically fix default formatting issues such as indentation, semicolons, and spacing, ensuring consistent code style across the project.

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,13 +1,21 @@
 // jest.config.mjs
 export default {
   testEnvironment: "jest-environment-jsdom",
-  transform: {
+  /* transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
-  moduleNameMapper: {
+ moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
-  },
+  },*/
   setupFilesAfterEnv: [
     "@testing-library/jest-dom", // Correct the import here
-  ]
+  ],
+  // Jest configuration
+  moduleNameMapper: {
+    "\\.css$": "<rootDir>/__mocks__/styleMock.js",
+    "\\.module\\.css$": "<rootDir>/__mocks__/styleMock.js", // Optional: for CSS modules
+  },
+  transform: {
+    "^.+\\.jsx?$": "babel-jest", // Ensure Babel processes JavaScript/JSX
+  },
 };
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,23 +2,30 @@ import React from "react";
 import "../styles/Homepage.css";
 
 const HomePage = () => {
-    return(
+  return (
     <div className="section1-container">
-        <div className = 'section1'>
-            <img className="cycle" src="./images/cycle.png"></img>
-            <div className="content-area">
-                <h1>Build.<br /> Share.<br /> Review.<br /> Repeat.</h1>
-                <p className="top-text">Showcase your work and connect with peers who can help you reach the next level.</p>
-                <div className="buton-area">
-                    <button class = "share-button">SHARE A PROJECT</button>
-                    <button class = "explore-button">EXPLORE PROJECTS</button>
-                </div>            
-            </div>
-            <img class="section-image" src="./images/sectionimg-1.png"></img>
+      <div className="section1">
+        <img className="cycle" src="./images/cycle.png"></img>
+        <div className="content-area">
+          <h1>
+            Build.
+            <br /> Share.
+            <br /> Review.
+            <br /> Repeat.
+          </h1>
+          <p className="top-text">
+            Showcase your work and connect with peers who can help you reach the
+            next level.
+          </p>
+          <div className="buton-area">
+            <button className="share-button">SHARE A PROJECT</button>
+            <button className="explore-button">EXPLORE PROJECTS</button>
+          </div>
         </div>
+        <img className="section-image" src="./images/sectionimg-1.png"></img>
+      </div>
     </div>
-
-    );
-}
+  );
+};
 
 export default HomePage;
