de0632fb3e6649ce50921db8504a34d1746b94a7
hdjoumdjeu <hermannpougoue1@gmail.com>
2024-12-19 02:18:08 -0600

    removed comment from jest config file and added a separate script for fixing lint

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,12 +1,6 @@
 // jest.config.mjs
 export default {
   testEnvironment: "jest-environment-jsdom",
-  /* transform: {
-    "^.+\\.jsx?$": "babel-jest",
-  },
- moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
-  },*/
   setupFilesAfterEnv: [
     "@testing-library/jest-dom", // Correct the import here
   ],
