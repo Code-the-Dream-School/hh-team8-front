b3465b38b84828fe47ab5b347463db3fc87598c8
hdjoumdjeu <hermannpougoue1@gmail.com>
2025-01-17 23:37:10 -0600

    authentification link to the backend

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,8 +5,75 @@ import { Field } from "./ui/field";
 import { InputGroup } from "./ui/input-group";
 import { LuUser } from "react-icons/lu";
 import { useState } from "react";
+import { Toaster, toaster } from "../components/ui/toaster";
 const Login = ({ onFormSwitch }) => {
   const [value, setValue] = useState("");
+  const url = "http://localhost:8001/api/v1/login";
+  const [userData, setUserData] = useState({
+    username: "",
+    password_hash: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleAuthentification = async (e) => {
+    e.preventDefault();
+    try {
+      console.log(userData);
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: userData.username,
+          password_hash: userData.password_hash,
+        }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        //alert(data.message);
+        toaster.create({
+          title: data.message,
+          type: "success",
+          duration: 4000,
+          action: {
+            label: "x",
+          },
+        });
+        //setAlertMessage(data.message);
+        console.log("Server Response:", data);
+        setUserData({
+          username: "",
+          password_hash: "",
+        });
+      } else {
+        console.error("Failed to add user");
+        toaster.create({
+          title: "Failed to add user",
+          type: "error",
+          action: {
+            label: "x",
+          },
+        });
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      toaster.create({
+        title: error,
+        type: "error",
+        action: {
+          label: "x",
+        },
+      });
+    }
+  };
   return (
     <VStack spacing={4} align="start" w="100%">
       <Box>
@@ -38,7 +105,13 @@ const Login = ({ onFormSwitch }) => {
             Username
           </Text>
           <InputGroup flex="1" startElement={<LuUser />}>
-            <Input placeholder="Username" w="308px" />
+            <Input
+              name="username"
+              value={userData.username}
+              onChange={handleChange}
+              placeholder="Username"
+              w="308px"
+            />
           </InputGroup>
         </Field>
         <Text marginTop="10px" fontSize="18px" fontWeight="400">
@@ -49,9 +122,10 @@ const Login = ({ onFormSwitch }) => {
           <PasswordInput
             label="Password"
             placeholder="Password"
-            value={value}
+            name="password_hash"
+            value={userData.password_hash}
+            onChange={handleChange}
             w="308px"
-            onChange={(e) => setValue(e.target.value)}
           />
         </Field>
         <Link
@@ -70,6 +144,7 @@ const Login = ({ onFormSwitch }) => {
           borderRadius="100px"
           marginBottom="20px"
           marginTop="32px"
+          onClick={handleAuthentification}
         >
           <Text
             fontWeight="800"
